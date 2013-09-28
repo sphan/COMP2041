360d1c574d02af92ffcc4a5ecda1931609f88035
Sandy <santhai.phan@gmail.com>
2013-09-29 08:44:12 +1000

    Has basic variables handling working, though it produces wrong output for printing variables in Python.

--- ass1/perl2python.pl
+++ ass1/perl2python.pl
@@ -6,6 +6,7 @@ use feature 'switch';
 # list of global variables
 my $input_file = "";
 my $output_content = "";
+my $has_variable_flag = 0;
 
 # check for arguments
 if (@ARGV > 0) {
@@ -14,14 +15,24 @@ if (@ARGV > 0) {
 
 open(F, "<$input_file") or die "Cannot open $input_file";
 while (my $line = <F>) {
+	$has_variable_flag = 0;
 	given($line) {
 		when (/#!\/usr\/bin\/perl/) {
 			$output_content .= "#!/usr/bin/python2.7 -u\n"
 		}
 		when (/print/) {
-			convert_print_statement($line);
+			$output_content .= convert_print_statement($line);
 		}
-		$output_content .= $line;
+		if (has_variable($line)) {
+			$output_content .= handle_variables($line);
+		}
+#		when (/\$\w+/) {
+		#	print "found a variable";
+#			handle_variables($line);
+#		}
+		when (/^$/s) {
+			$output_content .= $line;
+		}
 	}
 }
 
@@ -30,13 +41,32 @@ print $output_content;
 sub convert_print_statement {
 	my $line = $_[0];
 	$line = remove_semicolon($line);
-	$output_content .= "print \"";
+	my $output .= "print \"";
 	while ($line =~ /"(.+?)"/g) {
 		my $match = $1;
+		if (has_variable($match)) {
+			$match =handle_variables($match);
+		}
 		$match =~ s!\\[rn]?!!g;
-		$output_content .= $match;
+		$output .= $match;
 	}
-	$output_content .= "\"";
+	$output .= "\"";
+	return $output;
+}
+
+sub handle_variables {
+	my $line = $_[0];
+	$line = remove_semicolon($line);
+	$line =~ s/\$//g;
+	return $line;
+}
+
+sub has_variable {
+	my $line = $_[0];
+	if ($line =~ /\$\w+/) {
+		return 1;
+	}
+	return 0;
 }
 
 sub remove_semicolon {
