5356618f57b07f293c0a90cb93288f7da4850ee6
Sandy <santhai.phan@gmail.com>
2013-10-05 14:01:42 +1000

    Reimplemented using triggers to different functions according to the content of the line

--- ass1/perl2python.pl
+++ ass1/perl2python.pl
@@ -2,118 +2,82 @@
 use strict;
 use warnings;
 
-# variables
 my $input_file = "";
+my $header_content = "";
+my $main_content = "";
 my %syntax_table = ();
-my $output_content = "";
 
-if (@ARGV > 0) {
-	$input_file = $ARGV[0];
-}
+main();
 
-set_up_syntax_table();
-
-# open the file
-open(F, "<$input_file") or die "Cannot open $input_file";
-# read the file
-while (my $line = <F>) {
-	if ($line =~ /#!\/usr\/bin\/perl/) {
-		$output_content .= "#!/usr/bin/python2.7 -u\n";
-	} else {
-		# remove any semicolon at end of line
-		$line = remove_semicolon($line);
-		# split line up by "" into tokens
-		my @tokens = split(/("[^"]*")/, $line);
-		foreach my $token (@tokens) {
-			# remove any \n character
-			$token = remove_newline($token);
-			$token =~ s/(\)|\()//g;
-			$token =~ s/\s\{/\:/g;
-			$token =~ s/\}//g;
-#			print $token;
-			if ($token !~ /(".*?")/) { 
-				# look for the corresponding Python syntax
-				# for the token
-				my @subtokens = split(/\s/, $token);
-				foreach my $t (@subtokens) {
-					if (exists $syntax_table{$token}) {
-						$output_content .= $syntax_table{$token};
-					} elsif (has_variables($token)) {
-						$token = handle_variables($token);
-						$output_content .= $token;
-					} else {
-						$output_content .= $token;
-					}
-				}
-			} else {
-				if (has_variables($token)) {
-					$token =~ s/\"//g;
-					$token = handle_variables($token);
-				} elsif ($token =~ /\"\"/) {
-					next;
-				}
-				$output_content .= "$token";
-			}
+sub main {
+	if (@ARGV > 0) {
+		$input_file = $ARGV[0];
+	}
+	set_up_syntax_table();
+	open(F, "<$input_file") or die "Cannot open $input_file";
+	while (my $line = <F>) {
+		$line =~ s/;$//;
+		if ($line =~ /#!\/usr\/bin\/perl/) {
+			$header_content .= "#!/usr/bin/python2.7 -u\n";
+		} elsif ($line =~ /print/) {
+			eval $syntax_table{"print"};
+		} elsif ($line =~ /^\s*\#/ || $line =~ /^\s*$/) {
+			$main_content .= $line;
+		} else {
+			$line = handle_variable($line);
+			$main_content .= $line;
 		}
 	}
-}
-print $output_content;
-
-sub set_up_syntax_table {
-	$syntax_table{"print"} = "print";
-	$syntax_table{"while"} = "while";
-	$syntax_table{"<STDIN>"} = "sys.stdin.readline()";
-	$syntax_table{"eq"} = "==";
-	$syntax_table{"lt"} = "<";
-	$syntax_table{"gt"} = ">";
-	$syntax_table{"ge"} = ">=";
-	$syntax_table{"le"} = "<=";
-	$syntax_table{"ne"} = "!=";
-	$syntax_table{"=="} = "==";
-	$syntax_table{"<"} = "<";
-	$syntax_table{">"} = ">";
-	$syntax_table{">="} = ">=";
-	$syntax_table{"<="} = "<=";
-	$syntax_table{"!="} = "!=";
-	$syntax_table{"{"} = ":";
-	$syntax_table{".="} = "+=";
-	$syntax_table{"&&"} = "and";
-	$syntax_table{"||"} = "or";
-	$syntax_table{"!"} = "not";
-	$syntax_table{"}"} = "";
-	$syntax_table{"last"} = "break";
-	$syntax_table{"next"} = "continue";
-	$syntax_table{"if"} = "if";
-	$syntax_table{"elsif"} = "elif";
-	$syntax_table{"foreach"} = "for";
+	print $header_content . $main_content;
 }
 
-sub handle_variables {
-	my $var = $_[0];
-	if ($var =~ /[\$\@\%]\w+/) {
-		$var =~ s/[\$\@\%]//g;
+sub handle_variable {
+	my $line = $_[0];
+	if ($line =~ /[\$\@\%]+/) {
+		$line =~ s/[\$\@\%]+//g;
 	}
-	return $var;
+	return $line;
 }
 
-# return true if the given string
-# contains a variable, false otherwise.
-sub has_variables {
-	my $var = $_[0];
-	if ($var =~ /[\$\@\%]\w+/) {
+sub has_variable {
+	my $line = $_[0];
+	if ($line =~ /[\$\@\%]+/) {
 		return 1;
 	}
 	return 0;
 }
 
-sub remove_newline {
-	my $t = $_[0];
-	$t =~ s!\\[rn]?!!g;
-	return $t;
+sub handle_if_while {
+	my $line = $_[0];
+	$line = handle_variable($line);
+	$line =~ s/\(\)//g;
+	$line =~ s/ {/:/g;
+	$main_content .= $line;
 }
 
-sub remove_semicolon {
+sub handle_print {
 	my $line = $_[0];
-	$line =~ s/;//;
-	return $line;
+	$line =~ s/\\[rn]//g;
+	$line =~ s/\s*print\s*//g;
+	$main_content .= "print ";
+	print $line;
+	my @parts = split(/, /, $line);
+	my $count = 0;
+	foreach my $p (@parts) {
+		if (has_variable($p)) {
+			$p = handle_variable($p);
+			$p =~ s/\"//g;
+		} elsif ($count > 0 && $p ne "\"\"") {
+			$main_content .= ", ";
+		}
+		next if ($p eq "\"\"");
+		$main_content .= $p;
+		$count += 1;
+	}
+}
+
+sub set_up_syntax_table {
+	$syntax_table{"print"} = 'handle_print($line)';
+	$syntax_table{"eq"} = "==";
+	$syntax_table{"ne"} = "!=";
 }
