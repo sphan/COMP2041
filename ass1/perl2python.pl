5245065b3076a010fac3fd2c971b360d127b1928
Sandy <santhai.phan@gmail.com>
2013-10-07 12:32:27 +1100

    implemented for loops

--- ass1/perl2python.pl
+++ ass1/perl2python.pl
@@ -9,43 +9,59 @@ my %syntax_table = ();
 # flag to see if sys or re (libraries) is imported or not
 my $sys_imported = 0;
 my $re_imported = 0;
+my $fileinput_imported = 0;
 
-main();
+if (@ARGV > 0) {
+	$input_file = $ARGV[0];
+	read_from_file();
+} else {
+	main(<STDIN>);
+	read_from_stdin();
+}
 
-sub main {
-	if (@ARGV > 0) {
-		$input_file = $ARGV[0];
-	}
-	set_up_syntax_table();
+sub read_from_file {
 	open(F, "<$input_file") or die "Cannot open $input_file";
 	while (my $line = <F>) {
-		$line =~ s/;$//;
-		if ($line =~ /#!\/usr\/bin\/perl/) {
-			$header_content .= "#!/usr/bin/python2.7 -u\n";
-		} elsif ($line =~ /^\s*print/) {
-			handle_print($line);
-		} elsif ($line =~ /^\s*\#/ || $line =~ /^\s*$/) {
-			$main_content .= $line;
-		} elsif ($line =~ /if/ || $line =~ /while/) {
-			handle_if_while($line);
-		} elsif ($line =~ /}/) {
-			next;
-		} elsif ($line =~ /STDIN/) {
-			handle_stdin($line);
-		} elsif ($line =~ /chomp/) {
-			handle_chomp($line);
-		} elsif ($line =~ /join\(.*?\)/) {
-			handle_join($line);
-		} elsif ($line =~ /foreach/ || $line =~ /for/) {
-			handle_for_loops($line);
-		} else {
-			$line = handle_variable($line);
-			$line =~ s/last/break/g;
-			$line =~ s/next/continue/g;
-			$main_content .= $line;
-		}
+		main($line);
 	}
 	print $header_content . $main_content;
+}
+
+sub read_from_stdin {
+	while (my $line = <STDIN>) {
+		main($line);
+	}
+	print $header_content . $main_content;
+}
+
+sub main {
+	set_up_syntax_table();
+	my $line = $_[0];
+	$line =~ s/;$//;
+	if ($line =~ /#!\/usr\/bin\/perl/) {
+		$header_content .= "#!/usr/bin/python2.7 -u\n";
+	} elsif ($line =~ /^\s*print/) {
+		handle_print($line);
+	} elsif ($line =~ /^\s*\#/ || $line =~ /^\s*$/) {
+		$main_content .= $line;
+	} elsif ($line =~ /^\s*if/ || $line =~ /^\s*while/) {
+		handle_if_while($line);
+	} elsif ($line =~ /}/) {
+#		next;
+	} elsif ($line =~ /STDIN/) {
+		handle_stdin($line);
+	} elsif ($line =~ /chomp/) {
+		handle_chomp($line);
+	} elsif ($line =~ /join\(.*?\)/) {
+		handle_join($line);
+	} elsif ($line =~ /^\s*foreach/ || $line =~ /^\s*for/) {
+		handle_for_loops($line);
+	} else {
+		$line = handle_variable($line);
+		$line =~ s/last/break/g;
+		$line =~ s/next/continue/g;
+		$main_content .= $line;
+	}
 }
 
 sub handle_variable {
@@ -64,10 +80,17 @@ sub has_variable {
 	return 0;
 }
 
-sub insert_sys_import {
-	if ($sys_imported == 0) {
-		$header_content .= "import sys\n";
-		$sys_imported = 1;
+sub handle_imports {
+	my $lib = $_[0];
+	if ($lib =~ /sys/) {
+		if ($sys_imported == 0) {
+			$header_content .= "import sys\n";
+			$sys_imported = 1;
+		}
+	} elsif ($lib =~ /re/) {
+
+	} elsif ($lib =~ /fileinput/) {
+
 	}
 }
 
@@ -84,7 +107,7 @@ sub handle_stdin {
 	my $line = $_[0];
 	$line = handle_variable($line);
 	$line =~ s/<STDIN>/sys.stdin.readline()/;
-	insert_sys_import();
+	handle_imports("sys");
 	$main_content .= $line;
 }
 
@@ -109,9 +132,38 @@ sub handle_if_while {
 
 sub handle_for_loops {
 	my $line = $_[0];
-	my ($spaces) = /(\s*)foreach/;
-	my $contents = split(/\s+/, $line);
-
+	my ($var_to_loop) = $line =~ /for.*\((.*)\)\s*{/;
+	my $spaces = "";
+	my $temp;
+	if (($spaces) = $line =~ /(\s*)foreach/) {
+		$temp = $spaces;
+	}
+#	print $var_to_loop;
+	$temp .= "for ";
+	if ($var_to_loop =~ /.*;.*;.*/) {
+		my ($var) = $var_to_loop =~ /\$(\w+).*/;
+		my ($from, $to) = $var_to_loop =~ /(\d+);.*?(\d+);.*?/;
+		$main_content .= $temp . "$var in xrange($from, $to):\n";
+		return;
+	} else {
+		my ($args) = $line =~ /foreach\s*(.*)\s*\(.*\)\s*\{/;
+		my @vars = split(/\s+/, $args);
+		for (my $i = 0; $i < @vars; $i += 1) {
+			$temp .= handle_variable($vars[$i]);
+			$temp .= ", " if (@vars > 1 && $i + 1 != @vars);
+			$temp .= " ";
+		}
+	}
+	if ($var_to_loop =~ /(\d+)..(\d+)/) {
+		my $to = $2 + 1;
+		$temp .= "in xrange ($1, $to):";
+	} elsif (exists $syntax_table{$var_to_loop}) {
+		$temp .= "in $syntax_table{$var_to_loop}:";
+		handle_imports("sys") if ($syntax_table{$var_to_loop} =~ /sys/);
+	} elsif ($var_to_loop =~ /\$\w+/) {
+		$temp .= handle_variable($var_to_loop) . ":";
+	}
+	$main_content .= $temp . "\n";
 }
 
 sub handle_join {
@@ -124,7 +176,7 @@ sub handle_join {
 	for (my $i = 1; $i < @subContents; $i += 1) {
 		if (exists $syntax_table{$subContents[$i]}) {
 			$temp .= $syntax_table{$subContents[$i]};
-			insert_sys_import() if ($syntax_table{$subContents[$i]} =~ /sys/);
+			handle_imports("sys") if ($syntax_table{$subContents[$i]} =~ /sys/);
 		} else {
 			$temp .= $subContents[$i];
 		}
@@ -147,7 +199,6 @@ sub handle_print {
 	if ($line =~ /join\(.*?\)/) {
 		handle_join($line);
 		return;
-#		last;
 	}
 	my @parts = split(/(, )|(\. )/, $line);
 	my $count = 0;
@@ -169,32 +220,17 @@ sub set_up_syntax_table {
 	$syntax_table{"last"} = "break";
 	$syntax_table{"next"} = "continue";
 	$syntax_table{"foreach"} = "for";
-
+	$syntax_table{"{"} = ":";
 
 	# compound assignment
-#	$syntax_table{"+="} = "+=";
-#	$syntax_table{"-="} = "-=";
-#	$syntax_table{"*="} = "*=";
 	$syntax_table{"/="} = "//=";
-#	$syntax_table{"**="} = "**=";
 	$syntax_table{".="} = "+=";
 	$syntax_table{"x="} = "*=";
 	$syntax_table{"&&="} = "&=";
 	$syntax_table{"||="} = "|=";
 	$syntax_table{"^="} = "^+=";
-#	$syntax_table{"<<="} = "<<=";
-#	$syntax_table{">>="} = ">>=";
-#	$syntax_table{"&="} = "&=";
-#	$syntax_table{"|="} = "|=";
-#	$syntax_table{"^="} = "^=";
 
 	# comparison operators
-#	$syntax_table{"=="} = "==";
-#	$syntax_table{"!="} = "!=";
-#	$syntax_table{"<"} = "<";
-#	$syntax_table{"<="} = "<=";
-#	$syntax_table{">"} = ">";
-#	$syntax_table{">="} = ">=";
 	$syntax_table{"eq"} = "==";
 	$syntax_table{"ne"} = "!=";
 	$syntax_table{"lt"} = "<";
