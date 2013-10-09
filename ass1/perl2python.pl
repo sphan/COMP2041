4d33b3120e5aafa78f4e5fb434f358f32a88fc71
sphan <santhai.phan@gmail.com>
2013-10-09 21:07:59 +1100

    Implemented basic regex handling. Fixed some bugs

--- ass1/perl2python.pl
+++ ass1/perl2python.pl
@@ -39,34 +39,50 @@ sub read_from_stdin {
 sub main {
 	my $line = $_[0];
 	$line =~ s/;$//g;
+	$line_count++;
 	if ($line =~ /\#!\/usr\/bin\/perl -w/) {
 		$header_content .= "#!/usr/bin/python2.7 -u\n";
+		$line_count--;
+	# handle print statements
 	} elsif ($line =~ /^\s*print/) {
 		handle_print($line);
-		$line_count++;
+	# handle if else statements and while statements
 	} elsif ($line =~ /^\s*if/ || $line =~ /else/ || $line =~ /elsif/ || $line =~ /^\s*while/) {
 		handle_if_while($line);
-		$line_count++;
+	# skip if line is '}'
 	} elsif ($line =~ "}") {
+	# handle lines with chomp
 	} elsif ($line =~ /chomp/) {
 		handle_chomp($line);
-		$line_count++;
+	# handle lines with splits in them
 	} elsif ($line =~ /split/) {
 		handle_split($line);
-		$line_count++;
+	# handle for loops
 	} elsif ($line =~ /foreach/ || $line =~ /for/) {
 		handle_for_loop($line);
-		$line_count++;
+	# handle basic array operations such as push, pop
+	} elsif ($line =~ /push|pop|shift|unshift|reverse/) {
+		$line = handle_array_op($line);
+		$line .= "\n";
+		push @main_content, $line;
+	# handle regex
+	} elsif ($line =~ /\@/) {
+		$line = handle_array($line);
+		$line .= "\n";
+		push @main_content, $line;
+	} elsif ($line =~ /\=\~/) {
+		handle_regex($line);
+	# handle lines with join
 	} elsif ($line =~ /join/) {
 		$line = handle_join($line);
 		$line .= "\n";
 		push @main_content, $line;
-		$line_count++;
+	# handle lines with incrementations and decrementations
 	} elsif ($line =~ /\+\+/ || $line =~ /\-\-/) {
 		$line = handle_crement($line);
 		$line .= "\n";
 		push @main_content, $line;
-		$line_count++;
+	# handle lines with no reserved words at all
 	} else {
 		my ($spaces) = $line =~ /(\s*)\w/;
 		my @line_content = ();
@@ -83,7 +99,6 @@ sub main {
 		@line_content = join(" ", @line_content);
 		push @line_content, "\n";
 		push @main_content, @line_content;
-		$line_count++;
 		$unsure_line = $line_count;
 		# my %index;
 		# @index{@main_content} = (0..$#main_content);
@@ -120,18 +135,20 @@ sub handle_imports {
 	my $lib = $_[0];
 	if ($lib eq "sys") {
 		$header_content .= "import sys\n" if (!$sys_imported);
+		$sys_imported = 1;
 	} elsif ($lib eq "re") {
 		$header_content .= "import re\n" if (!$re_imported);
+		$re_imported = 1;
 	} elsif ($lib eq "fileinput") {
 		$header_content .= "import fileinput\n" if (!$fileinput_imported);
+		$fileinput_imported = 1;
 	}
 }
 
 sub handle_crement {
 	my $line = $_[0];
 	my $str = "";
-	print $line;
-	my ($space) = $line =~ /(\s*)\w/;
+	my ($space) = $line =~ /(\s*)/;
 	my ($var) = $line =~ /[\$\%\@](\w+)/;
 	if ($line =~ /\+\+/) {
 		$str .= $space . $var . " += 1";
@@ -150,7 +167,6 @@ sub handle_chomp {
 	push @line_content, $var;
 	push @line_content, " = ";
 	push @line_content, "$var.rstrip()\n";
-	@line_content = join(" ", @line_content);
 	push @main_content, @line_content;
 }
 
@@ -177,7 +193,77 @@ sub handle_split {
 }
 
 sub handle_regex {
-	
+	my $line = $_[0];
+	handle_imports("re");
+	my @line_content = ();
+	my ($space) = $line =~ /(\s*)/;
+	my ($var) = $line =~ /(\w+)\s+=~/;
+	my ($command) = $line =~ /(\w+)\/.*?\//;
+	my ($pattern, $replace_str, $flags);
+	push @line_content, $space;
+	if ($var) {
+		push @line_content, "$var = ";
+	}
+	push @line_content, "re.";
+	# print $command;
+	if ($command && $command =~ /s/) {
+		($pattern, $replace_str, $flags) = $line =~ /\/(.*?)\/(.*?)\/(.*?)/;
+		($flags) = $line =~ /\/\/(.*)/;
+		push @line_content, "sub";
+	} elsif ($command && $command eq "") {
+		($pattern, $flags) = $line =~ /\/(.*?)\/(.*?)/;
+		push @line_content, "search";
+	}
+	push @line_content, "(";
+	push @line_content, "r\'$pattern\'";
+	if ($command =~ /s/) {
+		push @line_content, ", \'$replace_str\'";
+	}
+	push @line_content, ", $var";
+	push @line_content, ")";
+	push @line_content, "\n";
+	push @main_content, @line_content;
+}
+
+sub handle_array_op {
+	my $line = $_[0];
+	$line = handle_variable($line);
+	my $string = "";
+	my ($space) = $line =~ /(\s*)\w/;
+	my ($op, $var2) =~ /\s*=\s*(\w+)\s*(\w+)/;
+	# print $var1 if ($var1);
+	print $op, $var2;
+	# my @components = split(/[\s]+/, $line);
+	# $components[1] =~ s/,//g;
+	# $string .= $space;
+	# if ($line =~ /reverse/ && @components == 2) {
+		# $string .= $components[1] . ".[::-1]";
+	# } elsif ($line =~ /reverse/ && @components > 2) {
+		# $string .= "$components[0] = " . $components[3] . ".reverse()";
+	# } elsif ($components[2] && $components[2] =~ /\(.*?\)/) {
+		# $components[2] =~ tr/\(\)/\[\]/;
+		# $string .= "$components[1].extend";
+	# } else {
+		# if (exists $syntax_table{$components[0]}) {
+			# $string .= "$components[1].$syntax_table{$components[0]}";
+		# }
+	# }
+	# $components[2] = "0, $components[2]" if ($components[0] =~ /unshift/);
+	# $components[2] = 0 if ($components[0] =~ /shift/);
+	# $string .= "($components[2])" if ($components[2] && $line !~ /reverse/);
+	return $string;
+}
+
+sub handle_array {
+	my $line = $_[0];
+	$line = handle_variable($line);
+	$line =~ tr/\(\)/\[\]/;
+	if ($line =~ /\.\./) {
+		my ($from, $to) = $line =~ /(\w+)\.\.(\w+)/;
+		$to++;
+		$line =~ s/(\w+)\.\.(\w+)/range\($from, $to\)/;
+	}
+	return $line;
 }
 
 sub handle_if_while {
@@ -185,25 +271,40 @@ sub handle_if_while {
 	my @line_content = ();
 	$line =~ s/\}\s//;
 	$line =~ s/elsif/elif/;
-	my ($space) = $line =~ /(\s*\w+)/;
+	my ($space) = $line =~ /(\s*)\w+/;
 	$line =~ s/$space\s*//;
 	push @line_content, $space;
 	# print $line;
-	my ($condition) = $line =~ /\((.*?)\)/;
-	$line =~ s/$condition//;
-	# print $condition;
-	my @components = split(/\s/, $condition);
-	foreach my $c (@components) {
-		if (exists $syntax_table{$c}) {
-			push @line_content, $syntax_table{$c};
-			handle_imports("sys") if ($syntax_table{$c} =~ /sys/);
-		} else {
-			push @line_content, handle_variable($c);
+	if ($line =~ /\<\>/) {
+		my $var = $line =~ /\$(\w+)/;
+		push @line_content, "for ";
+		push @line_content, "$var in fileinput.input()";
+		push @line_content, ":\n";
+		handle_imports("fileinput");
+	} elsif ($line =~ /<STDIN>/) {
+		my $var = $line =~ /\$(\w+)/;
+		push @line_content, "for ";
+		push @line_content, "$var in sys.stdin";
+		push @line_content, ":\n";
+		handle_imports("sys");
+	} else {
+		my ($condition) = $line =~ /\((.*?)\)/;
+		push @line_content, $line =~ /\s*(\w+)/;
+		$line =~ s/$condition//;
+		# print $condition;
+		my @components = split(/\s/, $condition);
+		foreach my $c (@components) {
+			if (exists $syntax_table{$c}) {
+				push @line_content, $syntax_table{$c};
+				handle_imports("sys") if ($syntax_table{$c} =~ /sys/);
+			} else {
+				push @line_content, handle_variable($c);
+			}
 		}
+		@line_content = join(" ", @line_content);
+		push @line_content, ":" if ($line =~ /\s*{/);
+		push @line_content, "\n";
 	}
-	@line_content = join(" ", @line_content);
-	push @line_content, ":" if ($line =~ /\s*{/);
-	push @line_content, "\n";
 	push @main_content, @line_content;
 }
 
@@ -367,4 +468,9 @@ sub set_up_syntax_table {
 	
 	$syntax_table{"undef"} = "None";
 	$syntax_table{"foreach"} = "for";
+	
+	$syntax_table{"push"} = "append";
+	$syntax_table{"pop"} = "pop";
+	$syntax_table{"shift"} = "shift";
+	$syntax_table{"unshift"} = "unshift";
 }
